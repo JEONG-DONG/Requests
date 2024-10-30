267ab084f9a33fa2c684735ace434b362aded69b
JeongDong Seo <sjd001@empas.com>
2024-10-30 11:01:36 +0900

    commit4

--- main_rbody_js.py
+++ main_rbody_js.py
@@ -6,38 +6,60 @@ from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    max_age=-1,  # Only for the sake of the example. Remove this in your own project.
+)
 
 #Pydantic Model 클래스는 반드시 BaseModel을 상속받아 생성. 
-
-    #description: str | None = None
-
-
-    #tax: float | None = None
-
-
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: Optional[float] = None
 
 #수행 함수의 인자로 Pydantic model이 입력되면 Json 형태의 Request Body 처리
-
+@app.post("/items/")
+async def create_item(item: Item):
+    print(f"*** {item}")
+    return item
 
 # Request Body의 Pydantic model 값을 Access하여 로직 처리
-
+@app.post("/items_tax/")
+async def create_item_tax(item: Item):
+    item_dict = item.dict()   # item.model_dump()와 동일
+    print(f"*** {item_dict}")
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
 
 # Path, Query, Request Body 모두 함께 적용. 
-# @app.put("/items/{item_id}")
-# async def update_item(item_id: int, item: Item, q: str | None = None):
-#     result = {"item_id": item_id, **item.dict()}
-#     if q:
-#         result.update({"q": q})
-#     return result
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.model_dump()}
+    
+    if q:
+        result.update({"q": q})
+    print("#### result:", result)
+    return result
 
-
-
-
-    #full_name: str | None = None
- 
 
 # 여러개의 request body parameter 처리. 
-# json 데이터의 이름값과 수행함수의 인자명이 같아야 함.  
+# json 데이터의 이름값과 수행함수의 인자명이 같아야 함. 
 
-
+class User(BaseModel):
+    username: str
+    full_name: str | None = None
+    
+@app.put("/items_mt/{item_id}")
+async def update_item_mt(item_id: int, item: Item, user: User):
+    results = {"item_id": item_id, "item": item, "user": user}
+    print("results:", results)
+    return results
