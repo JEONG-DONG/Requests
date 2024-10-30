31742bc4729ffdf53c0b9af41821e2c05b2e9203
JeongDong Seo <sjd001@empas.com>
2024-10-30 11:14:23 +0900

    commit5

--- main_request.py
+++ main_request.py
@@ -1,3 +1,53 @@
 from fastapi import FastAPI, Request
 
 app = FastAPI()
+
+@app.get("/items")
+async def read_item(request: Request):
+    client_host = request.client.host  # Get the client host
+    headers = request.headers   # Get the headers
+    query_params = request.query_params   # Get the query parameters
+    url = request.url   # Get the URL
+    path_params = request.path_params  # Get the path parameters
+    http_method = request.method  # Get the HTTP method
+    
+    print(f"Client Host: {client_host}")
+    return {
+            "client_host": client_host,
+            "headers": headers,
+            "query_params": query_params,
+            "path_params": path_params,
+            "url": str(url),
+            "http_method":  http_method
+        }
+
+@app.get("/items/{item_group}")
+async def read_item_p(request: Request, item_group: str):
+    client_host = request.client.host
+    headers = request.headers 
+    query_params = request.query_params
+    url = request.url
+    path_params = request.path_params
+    http_method = request.method
+
+    return {
+        "client_host": client_host,
+        "headers": headers,
+        "query_params": query_params,
+        "path_params": path_params,
+        "url": str(url),
+        "http_method":  http_method
+    }
+
+
+@app.post("/items_json/")
+async def create_item_json(request: Request):
+    data =  await request.json()  # Parse JSON body
+    print("received_data:", data)
+    return {"received_data": data}
+
+@app.post("/items_form/")
+async def create_item_form(request: Request):
+    data = await request.form() # Parse Form body
+    print("received_data:", data)
+    return {"received_data": data}
